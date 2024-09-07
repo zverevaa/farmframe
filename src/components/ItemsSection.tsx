d655c5d8f944314283dbf13ed4033b8ee266c86b
Kira Zvereva <angryvex@gmail.com>
2024-09-07 17:11:36 +0300

    Add show/hide to the items section

--- src/components/ItemsSection.tsx
+++ src/components/ItemsSection.tsx
@@ -1,11 +1,14 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import Items from "./Items";
+import { useState } from "react";
 
 enum itemTypes {
     warframes = "Warframes",
 }
 
 export default function ItemsSection({ data }) {
+    const [isOpened, setIsOpened] = useState(true);
+
     return (
         <section className="items-section">
             <div className="items-section__container">
@@ -13,12 +16,15 @@ export default function ItemsSection({ data }) {
                     {itemTypes.warframes.toUpperCase()}
                 </h2>
                 <TriangleDownIcon
-                    className="items-section__icon"
+                    onClick={() => setIsOpened(!isOpened)}
+                    className={`items-section__icon ${
+                        isOpened ? null : "items-section__icon--flipped"
+                    }`}
                     width={"32"}
                     height={"32"}
                 />
             </div>
-            <Items itemsType={itemTypes.warframes} data={data} />
+            {isOpened && <Items itemsType={itemTypes.warframes} data={data} />}
         </section>
     );
 }
