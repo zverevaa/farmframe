b2877384ded41433a9798e5eaec016a173feb05b
Kira Zvereva <angryvex@gmail.com>
2024-09-25 02:48:55 +0300

    Add horizontal bar view

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -5,25 +5,45 @@ type TItemProps = {
     isSelected: boolean;
 };
 
+const viewType = "bar";
+
 export default function Item({ itemData, onClick, isSelected }: TItemProps) {
     const { name, imageName } = itemData;
     return (
-        <div
-            onClick={onClick}
-            className={`item item--rendered ${
-                isSelected ? "item--selected" : ""
-            }`}
-        >
-            <div className="item__container">
-                <img
-                    className="item__img"
-                    src={`https://cdn.warframestat.us/img/${imageName}`}
-                    alt=""
-                />
-                <div className="item__name">
-                    {name.split(" ")[0].toUpperCase()}
+        <>
+            {viewType === "horizontal" && (
+                <div
+                    onClick={onClick}
+                    className={`item item--rendered ${
+                        isSelected ? "item--selected" : ""
+                    }`}
+                >
+                    <div className="item__container">
+                        <img
+                            className="item__img"
+                            src={`https://cdn.warframestat.us/img/${imageName}`}
+                            alt=""
+                        />
+                        <div className="item__name">
+                            {name.split(" ")[0].toUpperCase()}
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+            {viewType === "bar" && (
+                <div
+                    onClick={onClick}
+                    className={`item item--bar item--rendered ${
+                        isSelected ? "item--selected" : ""
+                    }`}
+                >
+                    <div className="item__container item__container--bar">
+                        <div className="item__name item__name--bar">
+                            {name.split(" ")[0].toUpperCase()}
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
