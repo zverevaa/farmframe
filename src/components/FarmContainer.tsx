b669c6515131c34083d52062217274cacd6aa061
Kira Zvereva <angryvex@gmail.com>
2024-09-25 07:26:30 +0300

    Add ItemSection into FarmContainer

--- src/components/FarmContainer.tsx
+++ src/components/FarmContainer.tsx
@@ -1,3 +1,19 @@
+import { ITEM_TYPES, SECTION_TITLES } from "../lib/constants";
+import ItemsSection from "./ItemsSection";
+
 export default function FarmContainer() {
-    return <div className="container">FarmContainer</div>;
+    return (
+        <div className="container">
+            <div className="container__body">
+                {ITEM_TYPES.map((type, i) => {
+                    return (
+                        <ItemsSection
+                            title={SECTION_TITLES[i]}
+                            data={filterData(type)}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
